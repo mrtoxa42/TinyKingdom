0893dc9aacc1ad715ab4b1e83aca2e1c0898af33
mrtoxa42 <betebet16@hotmail.com>
2024-04-12 18:39:41 +0300

    a
    a

--- Scripts/GameSystem/game_system.gd
+++ Scripts/GameSystem/game_system.gd
@@ -5,7 +5,7 @@ extends Node2D
 
 func _process(delta):
 	$CanvasLayer/ArmyFormationKnight.global_position = get_global_mouse_position()
-	$CanvasLayer/ArmyFormationArcher.global_position = $CanvasLayer/ArmyFormationKnight.global_position - Vector2(0,100)
+	$CanvasLayer/ArmyFormationArcher.global_position = get_global_mouse_position() - Vector2(25,25)
 
 	
 
